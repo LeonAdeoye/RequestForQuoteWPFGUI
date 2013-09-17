b0388d497908776505e02c6b9b0bc0220e966844
LeonAdeoye <leon.adeoye@gmail.com>
2013-09-17 22:17:04 +0900

    added enum description attribs and updated status enum xaml

--- RequestForQuoteInterfacesLibrary/Enums/StatusEnum.cs
+++ RequestForQuoteInterfacesLibrary/Enums/StatusEnum.cs
@@ -1,11 +1,18 @@
-﻿namespace RequestForQuoteInterfacesLibrary.Enums
+﻿using System.ComponentModel;
+
+namespace RequestForQuoteInterfacesLibrary.Enums
 {
     public enum StatusEnum
     {
+        [DescriptionAttribute("Pending pick up")]
         PENDING,
+        [DescriptionAttribute("Picked up")]
         PICKEDUP,
+        [DescriptionAttribute("Filled")]
         FILLED,
+        [DescriptionAttribute("Traded away")]
         TRADEDAWAY,
+        [DescriptionAttribute("Invalid")]
         INVALID
     }
 }
