82b841808daf69ed8c05a22da695830637170409
LeonAdeoye <leon.adeoye@gmail.com>
2013-10-02 21:26:42 +0900

    switched web service config from linkedlist to list.

--- RequestForQuoteServicesModuleLibrary/ServicesImplementation/OptionRequestPersistanceManagerImpl.cs
+++ RequestForQuoteServicesModuleLibrary/ServicesImplementation/OptionRequestPersistanceManagerImpl.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ServiceModel;
 using RequestForQuoteInterfacesLibrary.Constants;
 using RequestForQuoteInterfacesLibrary.ModelInterfaces;
@@ -9,37 +8,37 @@ using log4net;
 
 namespace RequestForQuoteServicesModuleLibrary.ServicesImplementation
 {
-    public class OptionRequestPersistanceManagerImpl : IOptionRequestPersistanceManager
-    {
-        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);        
-        private readonly RequestControllerClient requestControllerProxy  = new RequestControllerClient();
+	public class OptionRequestPersistanceManagerImpl : IOptionRequestPersistanceManager
+	{
+		private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);        
+		private readonly RequestControllerClient requestControllerProxy  = new RequestControllerClient();
 
-        public int SaveRequest(IRequestForQuote requestToSave)
-        {
-            var requestDetail = new requestDetailImpl();
-            if(requestToSave.Legs != null && requestToSave.Legs.Count > 0)
-            {
-                var legsArray = new optionDetailImpl[requestToSave.Legs.Count];
-                var legCount = 0;
-                foreach(var leg in requestToSave.Legs)
-                {
-                    legsArray[legCount++] = new optionDetailImpl()
-                        {
-                            isCall = leg.IsCall,
-                            legId = leg.LegId,
-                            isEuropean = leg.IsEuropean
-                            // TODO add more properties
-                        };
-                }
-                requestDetail.legs = new optionDetailListImpl() {optionDetailList = legsArray};
-            }
+		public int SaveRequest(IRequestForQuote requestToSave)
+		{
+			var requestDetail = new requestDetailImpl();
+			if(requestToSave.Legs != null && requestToSave.Legs.Count > 0)
+			{
+				var legsArray = new optionDetailImpl[requestToSave.Legs.Count];
+				var legCount = 0;
+				foreach(var leg in requestToSave.Legs)
+				{
+					legsArray[legCount++] = new optionDetailImpl()
+						{
+							isCall = leg.IsCall,
+							legId = leg.LegId,
+							isEuropean = leg.IsEuropean
+							// TODO add more properties
+						};
+				}
+				requestDetail.legs = new optionDetailListImpl() {optionDetailList = legsArray};
+			}
 
-            requestDetail.bookCode = requestToSave.BookCode;
-            requestDetail.request = requestToSave.Request;
+			requestDetail.bookCode = requestToSave.BookCode;
+			requestDetail.request = requestToSave.Request;
 			requestDetail.identifier =	requestToSave.Identifier; 			
 			requestDetail.clientId = requestToSave.Client.Identifier;  
 			requestDetail.isOTC =requestToSave.IsOTC; 
-		    requestDetail.status = requestToSave.Status.ToString(); //6
+			requestDetail.status = requestToSave.Status.ToString(); //6
 
 			requestDetail.tradeDate = requestToSave.TradeDate; 
 			requestDetail.expiryDate = requestToSave.ExpiryDate; //8
@@ -106,133 +105,133 @@ namespace RequestForQuoteServicesModuleLibrary.ServicesImplementation
 			requestDetail.totalPremium = requestToSave.TotalPremium;
 			requestDetail.pickedUpBy =	requestToSave.PickedUpBy; //58
 
-            try
-            {
-                return requestControllerProxy.save(requestDetail, RequestForQuoteConstants.MY_USER_NAME);
-            }
-            catch (FaultException exception)
-            {
-                log.Error("Failed to save request. exception thrown: " , exception);
-                return -1;
-            }            
-        }
+			try
+			{
+				return requestControllerProxy.save(requestDetail, RequestForQuoteConstants.MY_USER_NAME);
+			}
+			catch (FaultException exception)
+			{
+				log.Error("Failed to save request. exception thrown: " , exception);
+				return -1;
+			}            
+		}
 
-        public bool UpdateRequest(IRequestForQuote requestToUpdate)
-        {
-            var requestDetail = new requestDetailImpl();
-            if (requestToUpdate.Legs != null && requestToUpdate.Legs.Count > 0)
-            {
-                var legsArray = new optionDetailImpl[requestToUpdate.Legs.Count];
-                var legCount = 0;
-                foreach (var leg in requestToUpdate.Legs)
-                {
-                    legsArray[legCount++] = new optionDetailImpl()
-                    {
-                        isCall = leg.IsCall,
-                        legId = leg.LegId,
-                        isEuropean = leg.IsEuropean
-                        // TODO add more properties
-                    };
-                }
-                requestDetail.legs = new optionDetailListImpl() { optionDetailList = legsArray };
-            }
+		public bool UpdateRequest(IRequestForQuote requestToUpdate)
+		{
+			var requestDetail = new requestDetailImpl();
+			if (requestToUpdate.Legs != null && requestToUpdate.Legs.Count > 0)
+			{
+				var legsArray = new optionDetailImpl[requestToUpdate.Legs.Count];
+				var legCount = 0;
+				foreach (var leg in requestToUpdate.Legs)
+				{
+					legsArray[legCount++] = new optionDetailImpl()
+					{
+						isCall = leg.IsCall,
+						legId = leg.LegId,
+						isEuropean = leg.IsEuropean
+						// TODO add more properties
+					};
+				}
+				requestDetail.legs = new optionDetailListImpl() { optionDetailList = legsArray };
+			}
 
-            requestDetail.bookCode = requestToUpdate.BookCode;
-            requestDetail.request = requestToUpdate.Request;
-            requestDetail.identifier = requestToUpdate.Identifier;
-            requestDetail.clientId = requestToUpdate.Client.Identifier;
-            requestDetail.isOTC = requestToUpdate.IsOTC;
-            requestDetail.status = requestToUpdate.Status.ToString(); //6
+			requestDetail.bookCode = requestToUpdate.BookCode;
+			requestDetail.request = requestToUpdate.Request;
+			requestDetail.identifier = requestToUpdate.Identifier;
+			requestDetail.clientId = requestToUpdate.Client.Identifier;
+			requestDetail.isOTC = requestToUpdate.IsOTC;
+			requestDetail.status = requestToUpdate.Status.ToString(); //6
 
-            requestDetail.tradeDate = requestToUpdate.TradeDate;
-            requestDetail.expiryDate = requestToUpdate.ExpiryDate; //8
+			requestDetail.tradeDate = requestToUpdate.TradeDate;
+			requestDetail.expiryDate = requestToUpdate.ExpiryDate; //8
 
-            requestDetail.lotSize = requestToUpdate.LotSize;
-            requestDetail.multiplier = requestToUpdate.Multiplier;
-            requestDetail.contracts = requestToUpdate.Contracts;
-            requestDetail.quantity = requestToUpdate.Quantity; //12
+			requestDetail.lotSize = requestToUpdate.LotSize;
+			requestDetail.multiplier = requestToUpdate.Multiplier;
+			requestDetail.contracts = requestToUpdate.Contracts;
+			requestDetail.quantity = requestToUpdate.Quantity; //12
 
-            requestDetail.notionalMillions = requestToUpdate.NotionalMillions;
-            requestDetail.notionalFXRate = requestToUpdate.NotionalFXRate;
-            requestDetail.notionalCurrency = requestToUpdate.NotionalCurrency.ToString(); //15
+			requestDetail.notionalMillions = requestToUpdate.NotionalMillions;
+			requestDetail.notionalFXRate = requestToUpdate.NotionalFXRate;
+			requestDetail.notionalCurrency = requestToUpdate.NotionalCurrency.ToString(); //15
 
-            requestDetail.delta = requestToUpdate.Delta;
-            requestDetail.gamma = requestToUpdate.Gamma;
-            requestDetail.vega = requestToUpdate.Vega;
-            requestDetail.theta = requestToUpdate.Theta;
-            requestDetail.rho = requestToUpdate.Rho; //20
+			requestDetail.delta = requestToUpdate.Delta;
+			requestDetail.gamma = requestToUpdate.Gamma;
+			requestDetail.vega = requestToUpdate.Vega;
+			requestDetail.theta = requestToUpdate.Theta;
+			requestDetail.rho = requestToUpdate.Rho; //20
 
-            requestDetail.deltaNotional = requestToUpdate.DeltaNotional;
-            requestDetail.gammaNotional = requestToUpdate.GammaNotional;
-            requestDetail.vegaNotional = requestToUpdate.VegaNotional;
-            requestDetail.thetaNotional = requestToUpdate.ThetaNotional;
-            requestDetail.rhoNotional = requestToUpdate.RhoNotional; //25
+			requestDetail.deltaNotional = requestToUpdate.DeltaNotional;
+			requestDetail.gammaNotional = requestToUpdate.GammaNotional;
+			requestDetail.vegaNotional = requestToUpdate.VegaNotional;
+			requestDetail.thetaNotional = requestToUpdate.ThetaNotional;
+			requestDetail.rhoNotional = requestToUpdate.RhoNotional; //25
 
-            requestDetail.deltaShares = requestToUpdate.DeltaShares;
-            requestDetail.gammaShares = requestToUpdate.GammaShares;
-            requestDetail.vegaShares = requestToUpdate.VegaShares;
-            requestDetail.thetaShares = requestToUpdate.ThetaShares;
-            requestDetail.rhoShares = requestToUpdate.RhoShares; //30
+			requestDetail.deltaShares = requestToUpdate.DeltaShares;
+			requestDetail.gammaShares = requestToUpdate.GammaShares;
+			requestDetail.vegaShares = requestToUpdate.VegaShares;
+			requestDetail.thetaShares = requestToUpdate.ThetaShares;
+			requestDetail.rhoShares = requestToUpdate.RhoShares; //30
 
-            requestDetail.askFinalAmount = requestToUpdate.AskFinalAmount;
-            requestDetail.askFinalPercentage = requestToUpdate.AskFinalPercentage;
-            requestDetail.askImpliedVol = requestToUpdate.AskImpliedVol;
-            requestDetail.askPremiumAmount = requestToUpdate.AskPremiumAmount;
-            requestDetail.askPremiumPercentage = requestToUpdate.AskPremiumPercentage; //35
+			requestDetail.askFinalAmount = requestToUpdate.AskFinalAmount;
+			requestDetail.askFinalPercentage = requestToUpdate.AskFinalPercentage;
+			requestDetail.askImpliedVol = requestToUpdate.AskImpliedVol;
+			requestDetail.askPremiumAmount = requestToUpdate.AskPremiumAmount;
+			requestDetail.askPremiumPercentage = requestToUpdate.AskPremiumPercentage; //35
 
-            requestDetail.bidFinalAmount = requestToUpdate.BidFinalAmount;
-            requestDetail.bidFinalPercentage = requestToUpdate.BidFinalPercentage;
-            requestDetail.bidImpliedVol = requestToUpdate.BidImpliedVol;
-            requestDetail.bidPremiumAmount = requestToUpdate.BidPremiumAmount;
-            requestDetail.bidPremiumPercentage = requestToUpdate.BidPremiumPercentage; //40
+			requestDetail.bidFinalAmount = requestToUpdate.BidFinalAmount;
+			requestDetail.bidFinalPercentage = requestToUpdate.BidFinalPercentage;
+			requestDetail.bidImpliedVol = requestToUpdate.BidImpliedVol;
+			requestDetail.bidPremiumAmount = requestToUpdate.BidPremiumAmount;
+			requestDetail.bidPremiumPercentage = requestToUpdate.BidPremiumPercentage; //40
 
-            requestDetail.premiumAmount = requestToUpdate.PremiumAmount;
-            requestDetail.premiumPercentage = requestToUpdate.PremiumPercentage;
-            requestDetail.impliedVol = requestToUpdate.ImpliedVol; //43
+			requestDetail.premiumAmount = requestToUpdate.PremiumAmount;
+			requestDetail.premiumPercentage = requestToUpdate.PremiumPercentage;
+			requestDetail.impliedVol = requestToUpdate.ImpliedVol; //43
 
-            requestDetail.salesCreditAmount = requestToUpdate.SalesCreditAmount;
-            requestDetail.salesCreditPercentage = requestToUpdate.SalesCreditPercentage;
-            requestDetail.salesCreditCurrency = requestToUpdate.SalesCreditCurrency.ToString();
-            requestDetail.salesCreditFXRate = requestToUpdate.SalesCreditFXRate; //47
+			requestDetail.salesCreditAmount = requestToUpdate.SalesCreditAmount;
+			requestDetail.salesCreditPercentage = requestToUpdate.SalesCreditPercentage;
+			requestDetail.salesCreditCurrency = requestToUpdate.SalesCreditCurrency.ToString();
+			requestDetail.salesCreditFXRate = requestToUpdate.SalesCreditFXRate; //47
 
-            requestDetail.premiumSettlementCurrency = requestToUpdate.PremiumSettlementCurrency.ToString();
-            requestDetail.premiumSettlementDate = requestToUpdate.PremiumSettlementDate;
-            requestDetail.premiumSettlementDaysOverride = requestToUpdate.PremiumSettlementDaysOverride;
-            requestDetail.premiumSettlementFXRate = requestToUpdate.PremiumSettlementFXRate; //51
+			requestDetail.premiumSettlementCurrency = requestToUpdate.PremiumSettlementCurrency.ToString();
+			requestDetail.premiumSettlementDate = requestToUpdate.PremiumSettlementDate;
+			requestDetail.premiumSettlementDaysOverride = requestToUpdate.PremiumSettlementDaysOverride;
+			requestDetail.premiumSettlementFXRate = requestToUpdate.PremiumSettlementFXRate; //51
 
-            requestDetail.salesComment = requestToUpdate.SalesComment;
-            requestDetail.traderComment = requestToUpdate.TraderComment;
-            requestDetail.clientComment = requestToUpdate.ClientComment; //54
+			requestDetail.salesComment = requestToUpdate.SalesComment;
+			requestDetail.traderComment = requestToUpdate.TraderComment;
+			requestDetail.clientComment = requestToUpdate.ClientComment; //54
 
-            requestDetail.hedgePrice = requestToUpdate.HedgePrice;
-            requestDetail.hedgeType = requestToUpdate.HedgeType.ToString();
-            requestDetail.totalPremium = requestToUpdate.TotalPremium;
-            requestDetail.pickedUpBy = requestToUpdate.PickedUpBy; //58
+			requestDetail.hedgePrice = requestToUpdate.HedgePrice;
+			requestDetail.hedgeType = requestToUpdate.HedgeType.ToString();
+			requestDetail.totalPremium = requestToUpdate.TotalPremium;
+			requestDetail.pickedUpBy = requestToUpdate.PickedUpBy; //58
 
-            try
-            {
-                return requestControllerProxy.update(requestDetail, RequestForQuoteConstants.MY_USER_NAME);
-            }
-            catch (FaultException exception)
-            {
-                log.Error("Failed to update request. exception thrown: ", exception);
-                return false;
-            }
-        }
+			try
+			{
+				return requestControllerProxy.update(requestDetail, RequestForQuoteConstants.MY_USER_NAME);
+			}
+			catch (FaultException exception)
+			{
+				log.Error("Failed to update request. exception thrown: ", exception);
+				return false;
+			}
+		}
 
-        public IRequestForQuote GetRequest(int identifier, bool rePrice)
-        {
-            throw new System.NotImplementedException();
-        }
+		public IRequestForQuote GetRequest(int identifier, bool rePrice)
+		{
+			throw new System.NotImplementedException();
+		}
 
-        public List<IRequestForQuote> GetRequestMatchingAdhocCriteria(ISearch search, bool rePrice)
-        {
-            throw new System.NotImplementedException();
-        }
+		public List<IRequestForQuote> GetRequestMatchingAdhocCriteria(ISearch search, bool rePrice)
+		{
+			throw new System.NotImplementedException();
+		}
 
-        public List<IRequestForQuote> GetRequestMatchingExistingCriteria(string criteriaOwner, string criteriaDescriptionKey, bool rePrice)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
+		public List<IRequestForQuote> GetRequestMatchingExistingCriteria(string criteriaOwner, string criteriaDescriptionKey, bool rePrice)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
