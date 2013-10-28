a844bee92591fb7308e0fefb44cd40d9b3e6b60c
LeonAdeoye <leon.adeoye@gmail.com>
2013-10-28 23:41:25 +0900

    removed redundant files

--- RequestForQuoteServicesModuleLibrary/ServicesImplementation/IReportDataManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace RequestForQuoteServicesModuleLibrary.ServicesImplementation
-{
-    public interface IReportDataManager
-    {
-        void GetRequestCountPerCategory(string reportType, string categoryType, DateTime fromDate, int minimumCount);
-    }
-}
