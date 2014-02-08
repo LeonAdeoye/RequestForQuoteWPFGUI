92e03aa042f8d36d3c27313b8d456ecb98bd0384
LeonAdeoye <leon.adeoye@gmail.com>
2014-02-08 21:55:45 +0000

    Added JSON deserialize attributes to private members of user and group implementation classes

--- RequestForQuoteInterfacesLibrary/ModelImplementations/GroupImpl.cs
+++ RequestForQuoteInterfacesLibrary/ModelImplementations/GroupImpl.cs
@@ -1,29 +1,63 @@
-﻿using RequestForQuoteInterfacesLibrary.ModelInterfaces;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using RequestForQuoteInterfacesLibrary.ModelInterfaces;
 
 namespace RequestForQuoteInterfacesLibrary.ModelImplementations
 {
-    public class GroupImpl : IGroup
+    [DataContract]
+    [DebuggerDisplay("Group with group id {GroupId}, group name {GroupName}, and validity of {IsValid}.")]
+    public class GroupImpl : IGroup, INotifyPropertyChanged
     {
-        private int groupId;
-        private string groupName;
-        private bool isValid;
+        [DataMember] private int groupId;
+        [DataMember] private string groupName;
+        [DataMember] private bool isValid;
 
         public int GroupId
         {
-            get { return groupId; }
-            set { groupId = value; }
+            get
+            {
+                return groupId;
+            }
+            set
+            {
+                groupId = value;
+                NotifyPropertyChanged("GroupId");
+            }
         }
 
         public string GroupName
         {
-            get { return groupName; }
-            set { groupName = value; }
+            get
+            {
+                return groupName;
+            }
+            set 
+            { 
+                groupName = value;
+                NotifyPropertyChanged("GroupName");
+            }
         }
 
         public bool IsValid
         {
-            get { return isValid; }
-            set { isValid = value; }
+            get
+            {
+                return isValid;
+            }
+            set 
+            { 
+                isValid = value;
+                NotifyPropertyChanged("IsValid");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
--- RequestForQuoteInterfacesLibrary/ModelImplementations/UserImpl.cs
+++ RequestForQuoteInterfacesLibrary/ModelImplementations/UserImpl.cs
@@ -1,57 +1,120 @@
-﻿using RequestForQuoteInterfacesLibrary.ModelInterfaces;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+using RequestForQuoteInterfacesLibrary.ModelInterfaces;
 
 namespace RequestForQuoteInterfacesLibrary.ModelImplementations
 {
-    public class UserImpl : IUser
+    [DataContract]
+    [DebuggerDisplay("User with user id {UserId}, first name {FirstName}, last name {LastName}, " +
+                     "email address {EmailAddress}, location name {LocationName}, group id {GroupId}, and validity of {IsValid}.")]
+    public class UserImpl : IUser, INotifyPropertyChanged
     {
-        private string userId;
-        private string firstName;
-        private string lastName;
-        private string emailAddress;
-        private int groupId;
-        private string locationName;
-        private bool isValid;
+        [DataMember] private string userId;
+        [DataMember] private string firstName;
+        [DataMember] private string lastName;
+        [DataMember] private string emailAddress;
+        [DataMember] private int groupId;
+        [DataMember] private string locationName;
+        [DataMember] private bool isValid;
 
         public string UserId
         {
-            get { return userId; }
-            set { userId = value; }
+            get
+            {
+                return userId;
+            }
+            set
+            {
+                userId = value;
+                NotifyPropertyChanged("UserId");
+            }
         }
 
         public string FirstName
         {
-            get { return firstName; }
-            set { firstName = value; }
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+                NotifyPropertyChanged("FirstName");
+            }
         }
 
         public string LastName
         {
-            get { return lastName; }
-            set { lastName = value; }
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+                NotifyPropertyChanged("LastName");
+            }
         }
 
         public string EmailAddress
         {
-            get { return emailAddress; }
-            set { emailAddress = value; }
+            get
+            {
+                return emailAddress;
+            }
+            set
+            {
+                emailAddress = value;
+                NotifyPropertyChanged("EmailAddress");
+            }
         }
 
         public int GroupId
         {
-            get { return groupId; }
-            set { groupId = value; }
+            get
+            {
+                return groupId;
+            }
+            set
+            {
+                groupId = value;
+                NotifyPropertyChanged("GroupId");
+            }
         }
 
         public string LocationName
         {
-            get { return locationName; }
-            set { locationName = value; }
+            get
+            {
+                return locationName;
+            }
+            set
+            {
+                locationName = value;
+                NotifyPropertyChanged("LocationName");
+            }
         }
 
         public bool IsValid
         {
-            get { return isValid; }
-            set { isValid = value; }
+            get
+            {
+                return isValid;
+            }
+            set
+            {
+                isValid = value;
+                NotifyPropertyChanged("IsValid");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
